d1e69176d5b51d91e50820278c5d90e6443791e6
michaelajiri <michaelajiri@gmail.com>
2024-07-19 15:27:01 +0100

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as SockJS from 'sockjs-client';
+
+var SockJs = require("sockjs-client");
+var Stomp = require("stompjs");
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'websocket-front-end';
+export class AppComponent implements OnInit {
+  private stompClient: any;
+  notifications: string[] = [];
+
+  ngOnInit() {
+    const socket = new SockJS('http://localhost:8081/ws');
+    this.stompClient = Stomp.over(socket);
+
+    this.stompClient.connect({}, (frame: any) => {
+      this.stompClient.subscribe('/topic/notifications', (message: any) => {
+        this.showNotification(message.body);
+      });
+    });
+  }
+
+  showNotification(message: string) {
+    console.log('Received notification:', message);
+    this.notifications.push(message);
+  }
 }
